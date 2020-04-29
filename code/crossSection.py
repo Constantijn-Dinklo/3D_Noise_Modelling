4f52c56d82ece951f4136b91add6fcdcccf73105
Laurens van Rijssel <laurens.v.rijssel@gmail.com>
2020-04-29 18:03:32 +0200

    wrote initial setup, and find triangle function (untested)

---
+++ code/crossSection.py
@@ -0,0 +1,165 @@
+import sys
+import numpy as np
+
+# test data
+trs =  [[0, 1, 2, 1, -1, -1],
+        [1, 3, 2, 2, 0, -1],
+        [2, 3, 4, 3, -1, 1],
+        [3, 5, 4, 4, 2, -1],
+        [4, 5, 6, 5, -1, 3],
+        [5, 7, 6, 6, 4, -1],
+        [6, 7, 8, 7, -1, 5],
+        [7, 9, 8, -1, 6, -1]]
+
+vertices = [[0,0,0],
+            [1,1,0],
+            [2,0,0],
+            [3,1,0],
+            [4,0,0],
+            [5,1,0],
+            [6,0,0],
+            [7,1,0],
+            [8,0,0],
+            [9,1,0]]
+
+source = [0.75, 0.5, 0]
+receiver = [8.25, 0.5, 0]
+
+class Tin:
+    def __init__(self, trs, vts):
+        self.trs = trs   # triangles [v1, v2, v3, n1, n2, n3]
+        self.vts = vts   # set with vertex numbers used in this mesh
+
+    def side_test(self, pa, pb, pc):        #pa, pb, pc = index numbor of P
+        """
+        Explanation:
+        find whether pc is on the left (+/-) or on the right (+/-) of the line pa -> pb
+        ---------------
+        Input:
+        vertex indices edge of tr: pa, pb p_source = pc
+        ---------------
+        Output:
+        boolean, whether the point is in the triangle
+        """
+        return ((self.pts[pa][0] - self.pts[pc][0]) * 
+               (self.pts[pb][1] - self.pts[pc][1]) - 
+               (self.pts[pb][0] - self.pts[pc][0]) * 
+               (self.pts[pa][1] - self.pts[pc][1]))
+
+    def point_in_triangle(self, p_source, tr):
+        """
+        Explanation:
+        ---------------
+        Input:
+        ---------------
+        Output:
+        """
+        d1 = self.side_test(self.trs[tr][0], self.trs[tr][1], p_source)
+        d2 = self.side_test(self.trs[tr][1], self.trs[tr][2], p_source)
+        d3 = self.side_test(self.trs[tr][2], self.trs[tr][0], p_source)
+
+        if(d1 > 0 and d2 > 0 and d3 > 0): 
+            return True # the point is in the triangle
+        else:
+            return False
+
+    def find_source_triangle(self, tr_init, p_source):
+        """
+        Explanation:
+        Find the trinagle in which the p_source is located, by means of walking from tr_init to the right triangle
+        ---------------
+        Input:
+        tr_init: an (arbitrary) triangle id to start from
+        p_source: the source point
+        ---------------
+        Output:
+        triangle id of triangle underneath source point
+        """
+        tr = tr_init.copy()
+        while(True):
+            # do the side test for all sides, returns the value
+            d1 = self.side_test(self.trs[tr][0], self.trs[tr][1], p_source)
+            d2 = self.side_test(self.trs[tr][1], self.trs[tr][2], p_source)
+            d3 = self.side_test(self.trs[tr][2], self.trs[tr][0], p_source)
+
+            # If all side_tests are positive (point is either exactly on the edge, or on the inside)
+            if(d1 >= 0 and d2 >= 0 and d3 >= 0): 
+                return tr
+            # when one the the tests returns negative, we have to go to the enxt triangle, first find the direction to go to:
+            # turn it into a list, and get the minimum
+            d = [d1, d2, d3]
+            d_min = d.index(min(d))
+
+            # find the right neighbouring triangle to go to.
+            nbs = [5,3,4]
+            nb_index = nbs[d_min]
+            #if(d_min == 0): d_min = 5
+            #if(d_min == 1): d_min = 3 
+            #if(d_min == 2): d_min = 4
+
+            # get the index of the neighbour triangle
+            tr = self.trs[tr][nb_index]
+
+    def walk_straight_to_receiver(self, tr_source):
+        """
+        Explanation:
+        ---------------
+        Input:
+        ---------------
+        Output:
+        """
+        pass
+
+    def create_cross_section(self, tr_list):
+        """
+        Explanation:
+        ---------------
+        Input:
+        ---------------
+        Output:
+        """
+        pass
+
+def write_obj(obj_filename):
+    print("=== Writing {} ===".format(obj_filename))
+    
+    f_out = open(obj_filename, 'w')
+    for v in vertices:
+        f_out.write("v " + str(float(v[0])) + " " + str(float(v[1])) + " " + str(float(v[2])) + "\n")
+    
+    f_out.write("o 0\n")
+
+    for tr in trs:
+        tr[0] += 1
+        tr[1] += 1
+        tr[2] += 1
+        f_out.write("f " + str(tr[0]) + " " + str(tr[1]) + " " + str(tr[2]) + "\n")
+    f_out.close()
+
+def main(sys_args):
+    """
+    Explanation:
+    get the cross section from 2 given points
+    find the starting triangle
+    walk to the other point etc.
+    ---------------
+    Input:
+    2 points in [x,y,z]
+    ---------------
+    Output:
+    list?
+    """
+    # sys args will be: 
+    # run command: python crossSection.py ....
+
+    trs = Tin(trs, vertices)
+    # find source triangle
+    Tin.find_source_triangle(4, source):
+    # Walk to receiver, and save passing triangles
+    # interpolat height and distance
+
+    #write_obj("out.obj")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
