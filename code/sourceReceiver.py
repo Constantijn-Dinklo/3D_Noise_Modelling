09bc8a1ee3f673f7cacd2ede726f289a12445ce9
mprusti <maaritprusti@gmail.com>
2020-05-08 15:04:28 +0200

    second version, still hard coded source, maybe some line segments of the source are not correct

--- code/sourceReceiver.py
+++ code/sourceReceiver.py
@@ -1,64 +1,58 @@
 # Generate sources and receivers
 import math
-import xml.etree.ElementTree as ET
+import xml.etree.cElementTree as ET
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Read the GML file
-tree = ET.parse('/Users/mprusti/Documents/geo1101/wegvakgeografie_simplified.gml')
-root = tree.getroot()
-line_segments = [ ]
+hard_coded_source = (93550, 442000)
+
+def read_gml(path):
+    # input: path to a xml file
+    # output: returns the roots of the xml file
+    tree = ET.parse(path)
+    root = tree.getroot()
+    return root
+
 structured_segments = [ ]
-
-# Creates a list with line segments
-for child in root.iter():
-    if 'coordinates' in child.tag:
-        coordinates = child.text
-        line = coordinates.split()
-        line_segments.append(line)
-        for elem in line_segments:
-            if len(elem) > 2:
-                for i in range(len(elem)):
-                    if i < (len(elem)-1):
-                        first_el = elem[i]
-                        second_el = elem[i + 1]
-                        line_segments.append([first_el, second_el])
-                line_segments.remove(elem)
-
-temp_point = [ ]
-structured_lines = [ ]
-# input is a list with line segments
-# output is a single line segment that can be tested for intersection
-for segment in line_segments:
-    for point in segment:
-        coord = point.split(',')
-        gml_point = [coord[0], coord[1]]
-        if len(temp_point) < 2:
-            temp_point.append(gml_point)
-        if len(temp_point) == 2:
-            #print([temp_point[0], temp_point[1]])
-            structured_lines.append([temp_point[0], temp_point[1]])
-            temp_point.clear()
-
-angle = 2.0 * (math.pi / 180) 
-cnossos_radius = 2000.0
-base_angle = 0.0
-lines_per_circle = [ ]
-angle_st = 2.0
-start_st = 0.0
-
-hard_coded_source = [93450, 445000]
-hard_coded_line = [[93450, 445000], [94500, 43500]]
+def return_segments_source(root):
+    line_string = [ ] # replace lists inside function
+    line_float = [ ]
+    line_segments = [ ]
+    # input: root of the xml file
+    # output: list of line segments consisting of two points, however format not correct yet
+    for child in root.iter():
+        if "coordinates" in child.tag:
+            coordinates = child.text
+            line_string = coordinates.split()
+            if len(line_string) > 1:
+                for point in line_string:
+                    coord = point.split(',') 
+                    line_float.append((float(coord[0]), float(coord[1])))
+                for i in range(len(line_float)-1):
+                    first_el = line_float[i]
+                    second_el = line_float[i + 1]
+                    line = (first_el, second_el)
+                    if line not in line_segments:
+                        line_segments.append((first_el, second_el))
+    return line_segments
 
 def return_points_circle(centre, radius, radians):
-    # input = receiver point, radius and the angle size
-    # output = next point on circumsfere
+    # input: receiver point [x, y], radius and the angle size
+    # output: next point on circumsfere [x, y]
     x_next = centre[0] + radius * math.cos(radians)
     y_next = centre[1] + radius * math.sin(radians)
     next_point = [x_next, y_next]
     return next_point
 
+angle = 2.0 * (math.pi / 180) 
+cnossos_radius = 100.0
+base_angle = 0.0
+angle_st = 2.0
+start_st = 0.0
 def return_line_segments_receiver(centre, start, step, base):
-    # input = receiver point
-    # output = all receiver line segments
+    # input: receiver point [x, y], start, step size, basis angle in degrees
+    # output: a list of all receiver line segments
+    lines_per_circle = [ ]
     while start < 360:
         next_angle = base * (math.pi / 180)
         following = return_points_circle(hard_coded_source, cnossos_radius, next_angle)
@@ -67,40 +61,63 @@ def return_line_segments_receiver(centre, start, step, base):
         start += step
     return lines_per_circle
 
-intersected_points = [ ]
-def return_point_intersection(line1, line2):
-    # input = two line segments, line1 = (p1, p2), line2 = (p3, p4)
-    # output = intersection point of the two line segments
-    xdiff = (float(line1[0][0]) - float(line1[1][0]), float(line2[0][0]) - float(line2[1][0]))
-    ydiff = (float(line1[0][1]) - float(line1[1][1]), float(line2[0][1]) - float(line2[1][1]))
-
-    def det(a, b):
-        return float(a[0]) * float(b[1]) - float(a[1]) * float(b[0])
-
-    div = det(xdiff, ydiff)
-    d = (det(*line1), det(*line2))
-    if div != 0:
-        x = det(d, xdiff) / div
-        y = det(d, ydiff) / div
-        return [x, y]
+def line_intersect(line1, line2):
+    # input: line1 is the receiver line segments, line2 is the source line segment
+    # output: intersection point in a tuple
+    d = (line2[1][1] - line2[0][1]) * (line1[1][0] - line1[0][0]) - (line2[1][0] - line2[0][0]) * (line1[1][1] - line1[0][1])
+    if d:
+        uA = ((line2[1][0] - line2[0][0]) * (line1[0][1] - line2[0][1]) - (line2[1][1] - line2[0][1]) * (line1[0][0] - line2[0][0])) / d
+        uB = ((line1[1][0] - line1[0][0]) * (line1[0][1] - line2[0][1]) - (line1[1][1] - line1[0][1]) * (line1[0][0] - line2[0][0])) / d
+    else:
+        return
+    if not(0 <= uA <= 1 and 0 <= uB <= 1):
+        return
+    x = line1[0][0] + uA * (line1[1][0] - line1[0][0])
+    y = line1[0][1] + uA * (line1[1][1] - line1[0][1])
+    return x, y
 
 dict_intersection = { }
 list_intersection = [ ]
-count = 0
+def return_dictionary(receiver_list, source_list):
+    # input: two lists of line segments
+    # output: a dictionary in which the receiver point is the key and a list of intersection points as the corresponding value
+    for circle_line in receiver_list:
+        for struct_line in source_list:
+            point_intersection = line_intersect(circle_line, struct_line)
+            if point_intersection not in list_intersection and point_intersection is not None:
+                list_intersection.append(point_intersection)
 
-# Check for every source line segment whether it intersects with source line segments
-hard_coded_lines = return_line_segments_receiver(hard_coded_source, start_st, angle_st, base_angle)
-#print("circle lines", hard_coded_lines)
-#print("structured_lines", structured_lines)
-for circle_line in hard_coded_lines:
-    for struct_line in structured_lines:
-        #print("circle line:", circle_line, "with struct line:", struct_line)
-        point_intersection = return_point_intersection(circle_line, struct_line)
-        #print("this is their intersection:", intersection)
-        if point_intersection not in list_intersection and point_intersection is not None:
-            list_intersection.append(point_intersection)
+    dict_intersection[hard_coded_source] = list_intersection
+    return dict_intersection
 
-# Save all the intersection points in dictionary
-hard_coded_source = tuple(hard_coded_source)
-dict_intersection[hard_coded_source] = list_intersection
-print(dict_intersection)
+if __name__ == '__main__':
+    doc = read_gml('/Users/mprusti/Documents/geo1101/wegvakgeografie_simplified.gml') # now local, should be global in future
+    line_lst = return_segments_source(doc) # check
+    hard_coded_source_lines = return_line_segments_receiver(hard_coded_source, start_st, angle_st, base_angle) # check
+    intersected = return_dictionary(hard_coded_source_lines, line_lst)
+
+    # Plot the source line segments
+    x_source = [ ]
+    y_source = [ ]
+    for ln in line_lst:
+        x_source.append(ln[0][0])
+        x_source.append(ln[1][0])
+        y_source.append(ln[0][1])
+        y_source.append(ln[1][1])
+    plt.plot(x_source, y_source, c='g')
+
+    # Plot the receiver line segments
+    x_receiver = [ ]
+    y_receiver = [ ]
+    for lne in hard_coded_source_lines:
+        x_receiver.append(lne[0][0])
+        x_receiver.append(lne[1][0])
+        y_receiver.append(lne[0][1])
+        y_receiver.append(lne[1][1])
+    plt.plot(x_receiver, y_receiver, c='b')
+
+    # Plot the intersection points
+    list_intersected = np.array(intersected.get(hard_coded_source))
+    plt.scatter(list_intersected[:,0], list_intersected[:,1], c='r')
+
+    plt.show()
