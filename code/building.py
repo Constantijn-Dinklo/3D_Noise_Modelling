9823331887e140e68727ecb915b2b6f577ae31e7
NadHobeika <nadine_hob@hotmail.com>
2020-05-13 19:13:42 +0200

    Adjusted the Building class

--- code/building.py
+++ code/building.py
@@ -1,11 +1,12 @@
 
-from shapely.geometry import Polygon
+from shapely.geometry import shape
 
 
 class Building:
 
-    def __init__(self, id, uuid, geometry_type, geometry):
+    def __init__(self, id, bag_id, geometry, ground_level, roof_level):
         self.id = id
-        self.uuid = uuid
-        self.geometry_type = geometry_type
-        self.polygon = Polygon(geometry)
+        self.bag_id = bag_id
+        self.polygon = shape(geometry)
+        self.ground_level = ground_level
+        self.roof_level = roof_level
--- code/buildingManager.py
+++ code/buildingManager.py
@@ -1,33 +1,31 @@
-
 from building import Building
 
 import fiona
+
 
 class BuildingManager:
 
     def __init__(self):
         self.buildings = {}
 
-    def read_buildings_gpkg(self, path_to_gpkg):
-        with fiona.open(path_to_gpkg) as pkg:
-            for building_info in pkg:
+    def read_buildings_shp(self, path_to_shp):
+        with fiona.open(path_to_shp) as records:
+            for building_info in records:
+                building_id = building_info['id']
+                building_bag_id = building_info['properties']['bag_id']
+                b_geom_shape = building_info['geometry']
+                b_ground_level = building_info['properties']['h_maaiveld']
+                b_roof_level = building_info['properties']['h_dak']
 
-                feature_id = building_info['id']
-                building_id = building_info['properties']['id']
-                building_uuid = building_info['properties']['uuid']
-                b_geom_type = building_info['geometry']['type']
-                b_geom_coord = building_info['geometry']['coordinates']
-                b_geom_shape = b_geom_coord[0][0]
-                
-                building = Building(building_id, building_uuid, b_geom_type, b_geom_shape)
+                building = Building(building_id, building_bag_id, b_geom_shape, b_ground_level, b_roof_level)
 
                 self.buildings[building_id] = building
 
     def get_building(self, id):
         return self.buildings[id]
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     buildingManager = BuildingManager()
-    buildingManager.read_buildings_gpkg('input/37fz1_bodemvlakken_6.0.gpkg')
-    #print(buildingManager.buildings)
+    buildingManager.read_buildings_shp('input/lod13.shp')
+    # print(buildingManager.buildings)
