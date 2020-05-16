add2ffcac3efbe6a5ef50473dc2ba1cf47e16ab7
Denis Giannelli <denisgiannelli@live.it>
2020-05-16 16:48:43 +0200

    Reflection algorithm with 1st and 2nd order reflection.

--- code/simpleReflection.py
+++ code/simpleReflection.py
@@ -1,6 +1,7 @@
 import fiona
 import math
 import misc
+import time
 
 class ReflectionPath:
 
@@ -73,8 +74,54 @@ class ReflectionPath:
         x = line1[0][0] + uA * (line1[1][0] - line1[0][0])
         y = line1[0][1] + uA * (line1[1][1] - line1[0][1])
         return [x,y]
+    
+    def split_lineseg(self,n,lineseg):
+        """
+        Explanation: A function that takes a line segment and splits it into multiple sub-segments, according to a specific number.
+        ---------------
+        Input:
+        n: the number of line sub-segments in which lineseg will be divided into
+        lineseg: the line segment in matter [[x1,y1],[xn,yn]]
+        ---------------
+        Output:
+        ref_list: a list of all vertices of lineseg (polyline), including the two outermost and original ones.
+        [[x1,y1],[x2,y2],[x3,y3].....[xn,yn]]
 
-    def get_paths(self,s,r):
+        Obs: There is a problem with floating numbers: if n is too large, the last element of v_list will never be lineseg[1]. This
+        is why a while loop cannot be used here; it is best to keep a for loop.
+        """
+        delta_x = lineseg[1][0] - lineseg[0][0] # delta_x can be positive, negative or zero, depending on the direction of the line.
+        delta_y = lineseg[1][1] - lineseg[0][1] # delta_x can be positive, negative or zero, depending on the direction of the line.
+        vertex = lineseg[0]
+        ref_list = [vertex]
+        for el in range(n):
+            x = vertex[0] + delta_x/n
+            y = vertex[1] + delta_y/n
+            vertex = [x,y]
+            ref_list.append(vertex)
+        return ref_list
+
+    def get_candidate_point(self,n):
+        """
+        Explanation: A function that gets all the walls from f_dict and create candidate reflection points.
+        ---------------        
+        Input:
+        n: the number (int) foi segments in which the wall will be divided into.
+        ---------------
+        Output:
+        void
+        """
+        for bag_id in f_dict:
+            hoogte_abs = f_dict[bag_id]['hoogte_abs']
+            walls = f_dict[bag_id]['walls']
+            for wall in walls:
+                ref_list = self.split_lineseg(n,wall)
+                for point in ref_list:
+                    point.append(hoogte_abs)
+                    candidate = [wall[0],point,wall[1]]
+                    c_list.append(candidate)
+
+    def get_first_paths(self,s,r):
         """
         Explanation: A function that reads a source point and a receiver and computes all possible first-order reflection paths,
         according to buildings that are stored in f_dict (separate dictionary)
@@ -87,8 +134,8 @@ class ReflectionPath:
         A list of all (independent) points that are capable of reflecting the sound wave from source to receiver.:
         l = [ [ p1, p2, p3, .... pn ] , [ h1, h2, h3, .... hn ] ]
         such that:
-        p = [x(float),y(float)]
-        h = value(float)
+        p = coordinates of the reflection point [x(float),y(float)]
+        h = height value of the building in which the reflection point lies into (float)
         the n-th element of "p_list" corresponds to the n-th element of "h_list".
         """
         coords   = [ ]
@@ -101,14 +148,62 @@ class ReflectionPath:
                 test_s = misc.side_test( wall[0], wall[1], s[:2]) #s[:2] makes the function to ignore an eventual 'z' value.
                 if test_r > 0 and test_s > 0: # This statement guarantees that S-REF and REF-R are entirely outside the polygon.
                     s_mirror = self.get_mirror_point(s,self.get_line_equation(wall[0],wall[1]))
-                    ref = self.line_intersect(wall,[s_mirror,r])
+                    ref = self.line_intersect(wall,[s_mirror,r[:2]])
                     if type(ref) == list:
                         coords.append(ref)
                         heights.append(hoogte_abs)
                         ref_z = ref
                         ref_z.append(hoogte_abs)
-                        p_list.append([s,ref_z,r])
+                        p1_list.append([s,ref_z,r])
+        print('1st-order reflection. numer of paths:',len(coords))
         return [ coords, heights ] #[ [p1, p2, ..., pn], [h1, h2, ..., hn] ] 
+    
+    def get_second_paths(self,s,r,t):
+        """
+        Explanation: A function that reads a source point and a receiver and computes all possible SECOND-ORDER reflection paths,
+        according to buildings that are stored in f_dict (separate dictionary)
+        ---------------        
+        Input:
+        number n : number of sub-segments in wich the line will be divided.
+        source s    [x,y,(z)]
+        receiver r  [x,y,(z)]
+        ---------------
+        Output:
+        A list of all (independent) point lists that are capable of reflecting the sound wave from source to receiver.:
+        l = [ [ [p11, p12], [p21, p22], [p31, p32], .... [pn1, pn2] ] , [ [h11, h12], [h21, h22], [h31, h32], .... [hn1, hn2] ]
+        such that:
+        pn(1)(2) = coordinates of the reflection point [x(float),y(float)]
+        hn = height value of the building in which the reflection point lies into (float)
+        the n-th element of "p_list" corresponds to the n-th element of "h_list".
+        """
+        coords   = [ ]
+        heights  = [ ]
+        for candidate in c_list:
+            #ref_list = []
+            for bag_id in f_dict:
+                hoogte_abs = f_dict[bag_id]['hoogte_abs']
+                walls = f_dict[bag_id]['walls']
+                for wall in walls:
+                    test_c = misc.side_test( wall[0], wall[1], candidate[1][:2]) #r[:2] makes the function to ignore an eventual 'z' value.
+                    test_s = misc.side_test( wall[0], wall[1], s[:2]) #s[:2] makes the function to ignore an eventual 'z' value.
+                    if test_c > 0 and test_s > 0: # This statement guarantees that S-REF and REF-CANDIDATE are entirely outside the polygon.
+                        s_mirror = self.get_mirror_point(s,self.get_line_equation(wall[0],wall[1]))
+                        b = self.line_intersect(wall,[s_mirror,candidate[1][:2]])
+                        if type(b) == list:
+                            test_b = misc.side_test( candidate[0], candidate[2], b[:2])
+                            test_r = misc.side_test( candidate[0], candidate[2], r[:2])
+                            if test_b > 0 and test_r > 0:
+                                b_mirror = self.get_mirror_point(b,self.get_line_equation(candidate[0], candidate[2]))
+                                dist = math.sqrt(((b_mirror[0]-candidate[1][0])**2)+((b_mirror[1]-candidate[1][1])**2))
+                                if dist > 0.1:
+                                    if abs(misc.side_test( b_mirror, candidate[1][:2], r)) <= t:
+                                        coords.append([b,candidate[1][:2]])
+                                        heights.append([hoogte_abs,candidate[1][2]])
+                                        b_z = b
+                                        b_z.append(hoogte_abs)
+                                        p2_list.append([s,b_z,candidate[1],r])
+        print('2nd-order reflection. numer of paths:',len(coords))
+        return [ coords, heights ] #[ [ [p11, p12], [p21, p22], .... [pn1, pn2] ] , [ [h11, h12], [h21, h22], .... [hn1, hn2] ]
 
 def read_buildings(input_file,dictionary):
     """
@@ -157,7 +252,7 @@ def read_points(input_file,dictionary):
             dictionary[source_id] = coord_object#[:len(coord_object)-1]
     layer.close()
 
-def write_output(output_file,lista):
+def write_output_1st(output_file,lista):
     """
     Explanation: A function that writes a CSV file with the reflected paths. It is used for visualising the paths in QGIS.
     ---------------
@@ -183,29 +278,68 @@ def write_output(output_file,lista):
     fout.close()
     #MultiLineStringZ ((93528.02305619 441927.11005859 2.5, 93567.67848824 441908.81858497 0, 93539.68248698 441892 1.4))
 
+def write_output_2nd(output_file,lista):
+    """
+    Explanation: A function that writes a CSV file with the reflected paths. It is used for visualising the paths in QGIS.
+    ---------------
+    Input:
+    output_file: the directory/name of the csv file to be created.
+    lista: a list containing all the 1st order propagation paths in the following schema:
+    [ [source_x, source_y, source_z,] , [b_ref_x, b_ref_y, b_ref_z], [c_ref_x, c_ref_y, c_ref_z], [receiver_x, receiver_y, receiver_z] ] 
+    ---------------
+    Output:
+    void.
+    """
+    fout = open(output_file,'w')
+    line = 'fid \t geometry \n'
+    fout.write(str(line))
+    count = 0
+    for path in lista:
+        # path = [s,b_z,candidate,r]
+        count += 1
+        sou =   path[0]
+        b_ref = path[1]
+        c_ref = path[2]
+        rec =   path[3]
+        line = '%d \t MultiLineStringZ ((%f %f %f, %f %f %f, %f %f %f, %f %f %f)) \n' % (count,sou[0],sou[1],sou[2],b_ref[0],b_ref[1],b_ref[2],c_ref[0],c_ref[1],c_ref[2],rec[0],rec[1],rec[2]) 
+        fout.write(line)
+    fout.close()
+    #MultiLineStringZ ((93528.02305619 441927.11005859 2.5, 93567.67848824 441908.81858497 0, 93539.68248698 441892 1.4))
+
 if __name__ == "__main__":
+    start = time.time()
+
     f_dict = { }
     s_dict = { }
     r_dict = { }
-    p_list = [ ]
-    
+    c_list = [ ]
+    p1_list = [ ]
+    p2_list = [ ]
+
     # DATASETS FOR 'SCENARIO 000'
     read_buildings('//Users/denisgiannelli/Documents/DOCS_TU_DELFT/_4Q/GEO1101/06_DATA/01_Scenarios/scenario000/Building Data/lod10/p75/37fz1_lod10_p75/pand.gpkg',f_dict)
     read_points('//Users/denisgiannelli/Documents/DOCS_TU_DELFT/_4Q/GEO1101/06_DATA/01_Scenarios/scenario000/Sources/Sources1.gpkg',s_dict)
     read_points('//Users/denisgiannelli/Documents/DOCS_TU_DELFT/_4Q/GEO1101/06_DATA/01_Scenarios/scenario000/Receivers/Receivers1.gpkg',r_dict)
     
     reflection_path = ReflectionPath(s_dict,r_dict,f_dict)
-
+    reflection_path.get_candidate_point(10)
+    
     for source in s_dict:
         for receiver in r_dict:
             print('source:',source,'receiver',receiver)
-            print('number of paths:',len(reflection_path.get_paths(s_dict[source],r_dict[receiver])[0]))
-            for el in reflection_path.get_paths(s_dict[source],r_dict[receiver]):
-                print(el)
+            reflection_path.get_first_paths(s_dict[source],r_dict[receiver])
+            reflection_path.get_second_paths(s_dict[source],r_dict[source],1)
             print()
+    
+    print('len(p1_list)')
+    print(len(p1_list))
+    print()
+    print('len(p2_list)')
+    print(len(p2_list))
 
-    for path in p_list:
-        print(path)
-    print(len(p_list))
+    write_output_1st('//Users/denisgiannelli/Documents/DOCS_TU_DELFT/_4Q/GEO1101/06_DATA/01_Scenarios/scenario000/path_1st.csv',p1_list)
+    write_output_2nd('//Users/denisgiannelli/Documents/DOCS_TU_DELFT/_4Q/GEO1101/06_DATA/01_Scenarios/scenario000/path_2nd_nXX_t1.csv',p2_list)
 
-    write_output('//Users/denisgiannelli/Documents/DOCS_TU_DELFT/_4Q/GEO1101/06_DATA/01_Scenarios/scenario000/path.csv',p_list)
+    end = time.time()
+    processing_time = end - start
+    print('processing time:',round(processing_time,2),'s')
