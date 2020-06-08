79f49f57deea2947eeaf66505fce832fa2361ebb
ConstantijnDinklo <cos.dinklo@gmail.com>
2020-06-08 10:57:27 +0200

    Receivermanager.py

---
+++ code/receiverManager.py
@@ -0,0 +1,22 @@
+import fiona
+
+from receiverPoint import ReceiverPoint
+
+class ReceiverManager:
+
+    def __init__(self):
+        self.receiver_points = {}
+
+    def read_receiver_points(self, file_path):
+        with fiona.open(file_path) as shape: #Open the receiver points shapefile
+            for elem in shape:
+                geometry = elem["geometry"]
+                rec_pt_coords = geometry["coordinates"]
+                rec_pt = ReceiverPoint(rec_pt_coords)
+                self.receiver_points[rec_pt_coords] = rec_pt
+
+    def determine_source_points(self, source_lines):
+        #Go through all the receiver points and get their possible source points
+        for rec_pt_coords in self.receiver_points:
+            rec_pt = self.receiver_points[rec_pt_coords]
+            rec_pt.find_intersection_points(source_lines)
